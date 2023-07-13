95a6493f3665170821390436a5985e6b61d2ff47
yasmen <yasmen.khaled2000@gmail.com>
2023-07-13 14:55:18 +0200

    C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -A program that prints a line to standard error.
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piese of art is useful\" - Dora Korpare, 2015-10-19\n", stdout);
+return(1);
+}
