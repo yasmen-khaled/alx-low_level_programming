8ec73612f2eb323ccf70c7e4b65aae40f66af3c4
yasmen <yasmen.khaled2000@gmail.com>
2023-07-13 08:01:53 +0200

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main (void){
+/**
+ * main -A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
