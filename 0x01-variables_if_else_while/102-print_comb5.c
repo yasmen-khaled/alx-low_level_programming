1c4964c161cd17367a54fc92adf540dbe7bbcbd9
yasmen <yasmen.khaled2000@gmail.com>
2023-07-16 20:34:29 +0200

    c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,29 +5,27 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{
-	int i, j;
- for (i = 0; i <= 99; i++)
+{	
+int i, j;
+for (i = 0; i <= 99; i++)
 { 
-
-	for (j = 0; j <= 99; j++)
-	{
-		if (i < j && i != j)
-		{ 
-			putchar ((i / 10) + '0');
-			putchar ((i % 10) + '0');
-			putchar(' ');
-			putchar ((j / 10) + '0');
-			putchar ((j / 10) + '0');
-			if (i != 98 || j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-
-	}
- }  
+for (j = 0; j <= 99; j++)
+{
+if (i < j && i != j)
+{ 
+putchar ((i / 10) + '0');
+putchar ((i % 10) + '0');
+putchar(' ');
+putchar ((j / 10) + '0');
+putchar ((j / 10) + '0');
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}  
 putchar('\n');
 return (0);
 }
