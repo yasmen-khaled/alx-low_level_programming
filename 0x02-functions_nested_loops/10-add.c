e99c6e39267da2aeb82fb16cb44c8336d7bfdd98
yasmen <yasmen.khaled2000@gmail.com>
2023-07-18 23:55:07 +0200

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds tow values
+ * @1: The first number to be computed
+ * @2: The second number to be computed
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2)
+}
