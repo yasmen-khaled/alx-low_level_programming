8396aec344ee4dae136ed3733283312a3e13c379
yasmen <yasmen.khaled2000@gmail.com>
2023-07-18 22:44:39 +0200

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,14 +8,14 @@
 void jack_bauer(void)
 
 {
-int a, b, c ,d; 
-for (a= 0; a <= 2; a++)
+int a, b, c, d;
+for (a = 0; a <= 2; a++)
 {
-for (b = 0 ; b <=9; b++)
+for (b = 0 ; b <= 9; b++)
 {
 if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
-for (c = 0; c <= 5; c++ )
+for (c = 0; c <= 5; c++)
 {
 for (d = 0; c <= 9; d++)
 {
