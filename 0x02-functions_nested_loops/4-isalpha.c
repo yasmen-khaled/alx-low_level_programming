d5019c8fc5086d6b7bdf55ec7801cdb476a5d2bd
yasmen <yasmen.khaled2000@gmail.com>
2023-07-18 20:25:05 +0200

    c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,12 +5,13 @@
  *@c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
-I
+
 int _islower(int c)
 {
-if (c >= 97 && c <= 122)
+if ((c >= 65 &&  c <= 97) || (c >= 97 && c <= 122))
 {
-return (1);
+return (1); 
 }
 return (0);
 }
+
