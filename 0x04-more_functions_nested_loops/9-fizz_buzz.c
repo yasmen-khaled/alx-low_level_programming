80d5f8a930f5c038a456e5639f7cabeb14f56cb2
yasmen <yasmen.khaled2000@gmail.com>
2023-07-21 00:35:03 +0200

    fun

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 /**
  * main - print numbers 1 - 100 followed by a new line
- * multiples of 3 print Fizz
- * multiples of 5 print Buzz
- * multiples of 3 and 5 print FizzBuzz
+ * numbers that are  multiples of 3 print Fizz
+ * numbers that are multiples of 5 print Buzz
+ * numbers that are multiples of 3 and 5 print FizzBuzz
+ * each number and word to be separated by space
  *
  * Return: Always 0 (Success)
  */
