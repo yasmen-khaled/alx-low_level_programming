375bed1e5c47b31d595428987344d78fd2cab773
yasmen <yasmen.khaled2000@gmail.com>
2023-07-21 02:32:31 +0200

    c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,18 +9,18 @@
 
 void more_numbers(void)
 {
-int num, row, count;
-for (row = 1; row <= 10; row++)
+int a, b, c;
+for (b = 1; b <= 10; b++)
 {
-for (count = 0; count <= 14; count++)
+for (c = 0; c <= 14; c++)
 {
-num = count;
-if (count > 9)
+a = c;
+if (c > 9)
 {
 _putchar(1 + 48);
-num = count % 10;
+a = c % 10;
 }
-_putchar(num + 48);
+_putchar(a + 48);
 }
 _putchar('\n');
 }
