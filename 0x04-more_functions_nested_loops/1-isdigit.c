fa5877ef07ca7d8b72958001bd17738363d200cb
yasmen <yasmen.khaled2000@gmail.com>
2023-07-21 02:25:26 +0200

    c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,14 +3,14 @@
 /**
  * _isdigit - check if input is digit between 0 - 9
  *
- * @c: input
+ * @j: input
  *
  * Return: 1 if its uppercase, 0 if not
  */
 
-int _isdigit(int c)
+int _isdigit(int j)
 {
-if (c >= 48 && c <= 57)
+if (j >= 48 && j <= 57)
 return (1);
 else
 return (0);
