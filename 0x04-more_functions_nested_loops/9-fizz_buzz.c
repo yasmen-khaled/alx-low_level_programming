d46f3781ba013a7db533c1db6885d047b9221109
yasmen <yasmen.khaled2000@gmail.com>
2023-07-21 17:02:57 +0200

    c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,25 +8,28 @@
  *
  */
 
-int main()
+int main(void)
 {
 int i;
+
 for (i = 1; i <= 100; i++)
 {
-
-if (i % 3 == 0 && i % 5 == 0)
+if (i % 3 == 0)
 {
-rintf("FizzBuzz\n");
+printf("Fizz ");
 }
-else if (i % 3 == 0){
-printf("Fizz\n");
+else if (i % 5 == 0)
+{
+printf("Buzz ");
 }
-else if (i % 5 == 0){
-printf("Buzz\n");
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
 }
 else
 {
-printf("%d\n", i);
+printf("%d ", i);
+}
 }
 return (0);
 }
