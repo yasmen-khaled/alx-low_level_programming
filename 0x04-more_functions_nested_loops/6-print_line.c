a10f597e32b7b557847b9ea95b562a80ca799caf
yasmen <yasmen.khaled2000@gmail.com>
2023-07-21 02:34:13 +0200

    c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,18 +3,18 @@
 /**
  * print_line - print a straight line
  * 
- * @n: is the number of times the _ character
+ * @k: is the number of times the _ character
  *     should be printed
  */
 
 void print_line(int n)
 {
-int lnChr;
-if (n <= 0)
+int i;
+if (k <= 0)
 _putchar('\n');
 else
 {
-for (lnChr = 1; lnChr <= n; lnChr++)
+for (i = 1; i <= n; i++)
 _putchar('_');
 _putchar('\n');
 }
