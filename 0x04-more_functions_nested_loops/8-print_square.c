c09701f91bc1dfa7fc6dfeaa020630545f5f137c
yasmen <yasmen.khaled2000@gmail.com>
2023-07-21 16:21:03 +0200

    c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,14 +12,13 @@
 void print_square(int size)
 {
 int i, x;
-for (i = 0; i < size; i++)
+for (i = 1; i <= size; i++)
 {
-for (x = 0; x < size; x++)
+for (x = 1; x <= size; x++)
 {
 _putchar('#');
 }
 _putchar('\n');
 }
-if (size <= 0)
-_putchar(\n);
+return (0);
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,12 +2,10 @@
 
 /**
  * main - print numbers 1 - 100 followed by a new line
- * numbers that are  multiples of 3 print Fizz
- * numbers that are multiples of 5 print Buzz
- * numbers that are multiples of 3 and 5 print FizzBuzz
- * each number and word to be separated by space
- *
  * Return: Always 0 (Success)
+ *
+ *Describe: prints the numbers from 1 to 100, followed by a new line.
+ *
  */
 
 int main(void)
@@ -17,7 +15,7 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && !(i % 5 == 0))
 printf("Fizz");
-else if (i % 5 == 0 && !(i % 5 == 0 ))
+else if (i % 5 == 0 && !(i % 5 == 0))
 printf("Buzz");
 else if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz");
