d8324b8a84030df3136e8822a976b9d91f598c53
yasmen <yasmen.khaled2000@gmail.com>
2023-07-21 02:40:31 +0200

    c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,8 +4,8 @@
  * print_line - print a straight line
  * 
  * @k: is the number of times the _ character
- *     should be printed
- */
+ * should be printed
+*/
 
 void print_line(int k)
 {
@@ -14,7 +14,7 @@ if (k <= 0)
 _putchar('\n');
 else
 {
-for (i = 1; i <= n; i++)
+for (i = 1; i <= k; i++)
 _putchar('_');
 _putchar('\n');
 }
