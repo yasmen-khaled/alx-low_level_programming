19ef1a16f641cce06613c318e3d58142aa3c453f
yasmen <yasmen.khaled2000@gmail.com>
2023-07-24 12:18:37 +0200

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - pointer
+ *@ptr: is the pointer
+ *
+ * Description: functin that takes a pointer to an int as prameter points to 98
+ */
+
+void main(int *ptr)
+{
+	*ptr = 98;
+}
