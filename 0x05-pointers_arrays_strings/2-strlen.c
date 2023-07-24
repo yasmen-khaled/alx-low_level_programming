eec88290b78b6b516dad51b6d9a653dcb65a9af6
yasmen <yasmen.khaled2000@gmail.com>
2023-07-24 13:21:53 +0200

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ *
+ * @s: string
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+while (*ptr != '\0')
+{
+length++;
+ptr++;
+}
+return (length);
+}
