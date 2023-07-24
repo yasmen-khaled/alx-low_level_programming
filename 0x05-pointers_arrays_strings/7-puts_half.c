fbece033648478afa058759f496ea4e04f12aa36
yasmen <yasmen.khaled2000@gmail.com>
2023-07-24 20:19:22 +0200

    puts-half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - function tahat prints half of string
+ * @str: input
+ *
+ * Return: half of print
+ */
+
+void puts_half(char *str)
+{
+	int i, x;
+	int k = 0;
+
+	for (i = o; str[a] != '\0'; i++)
+	k++;
+	x = (k / 2);
+
+	if ((k % 2) == 1)
+	x = ((k + 1) / 2);
+
+	for (i = x; str[a] != '\0'; i++)
+	_putchar(str[a]);
+	_putchar('\n');
+}
