e3ebb025fb03018572928912bd2b8a7a0f7008b8
yasmen <yasmen.khaled2000@gmail.com>
2023-07-26 22:44:24 +0200

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - compairr string
+ * _strcmp - compairr string value
  * @s1: input
  * @s2: input
  *
@@ -14,7 +14,7 @@ int x;
 x = 0;
 while (s1[x] !=  '\0' && s2[x] != '\0')
 {
-	if (s1[x] != s2[x] != '\0')
+	if (s1[x] != s2[x])
 	{
 	return (s1[x] - s2[x]);
 	}
