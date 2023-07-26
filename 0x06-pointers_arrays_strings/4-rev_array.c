e15498feaad0bb7480f43b409f881fada6898300
yasmen <yasmen.khaled2000@gmail.com>
2023-07-27 00:21:53 +0200

    rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: numbers
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x, y;
+
+	for (x = 0; x < n--; x++)
+	{
+	y = a[x];
+	a[x] = a[n];
+	a[n] = y;
+	}
+}
