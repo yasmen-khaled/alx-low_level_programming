ee2e944a6df044179455dc0005804bb46e281072
yasmen <yasmen.khaled2000@gmail.com>
2023-07-26 22:04:45 +0200

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *  _strncat - concatenates tow strings
+ *
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ *
+ * Return: dest
+ */
+
+char *strncat(char *dest, char *src, int n)
+{
+	int x, k;
+
+	x = 0;
+	while (dest[x] != '\0')
+	{
+	x++;
+	}
+	k = 0;
+	while (k < n && src[k] != '\0')
+	{
+	dest[x] = src[k];
+	x++;
+	k++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
