004043d7e753beab5b5168245928dcf9f981ec32
yasmen <yasmen.khaled2000@gmail.com>
2023-07-31 21:05:00 +0200

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0;
+	int i;
+
+	while (*s)
+	{
+	for (i = 0; accept[i]; i++)
+	{
+	if (*s == accept[i])
+	{
+	x++;
+	break;
+	}
+	else if (accept[i + 1] == '\0')
+	return (x);
+	}
+	s++;
+	}
+	return (x);
+}
