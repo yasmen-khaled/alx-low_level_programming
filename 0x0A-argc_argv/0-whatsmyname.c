1e8be9086e063406ff8092a55eb39f229683ea83
yasmen <yasmen.khaled2000@gmail.com>
2023-08-04 16:17:33 +0200

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
+ * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
  *
@@ -11,8 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-(void) argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
