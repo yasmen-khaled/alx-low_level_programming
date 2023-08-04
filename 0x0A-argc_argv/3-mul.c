a67f31de5f7f9f00825783283dcb4a2c1b76157a
yasmen <yasmen.khaled2000@gmail.com>
2023-08-05 00:18:51 +0200

    mult

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -6,13 +7,14 @@
  *
  * Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
-	int a, b, c, len, f, digit;
+	int i, d, n, len, f, digit;
 
-	a = 0;
-	b = 0;
-	c = 0;
+	i = 0;
+	d = 0;
+	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
@@ -20,29 +22,29 @@ int _atoi(char *s)
 	while (s[len] != '\0')
 		len++;
 
-	while (a < len && f == 0)
+	while (i < len && f == 0)
 	{
-		if (s[a] == '-')
-			++b;
+		if (s[i] == '-')
+			++d;
 
-		if (s[a] >= '0' && s[a] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[a] - '0';
-			if (b % 2)
+			digit = s[i] - '0';
+			if (d % 2)
 				digit = -digit;
-			c = c * 10 + digit;
+			n = n * 10 + digit;
 			f = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
-		a++;
+		i++;
 	}
 
 	if (f == 0)
 		return (0);
 
-	return (c);
+	return (n);
 }
 
 /**
