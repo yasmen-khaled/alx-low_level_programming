c35c55983d3abb8499c097cb2f85f5894b489b33
yasmen <yasmen.khaled2000@gmail.com>
2023-08-30 01:02:11 +0200

    find-loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -13,15 +13,15 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *s = head;
 	listint_t *f = head;
 
-	if (!head)
-		return (NULL);
+if (!head)
+return (NULL);
 
-	while (s && f && f->next)
-	{
-		f = f->next->next;
-		s = s->next;
-		if (f == s)
-		{
+while (s && f && f->next)
+{
+f = f->next->next;
+s = s->next;
+if (f == s)
+{
 	s = head;
 	while (s != f)
 	{
