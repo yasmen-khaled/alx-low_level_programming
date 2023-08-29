e979c22ce49cb5b70923b3d20669a33e2c9aca13
yasmen <yasmen.khaled2000@gmail.com>
2023-08-29 20:47:48 +0200

    free-listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,14 +3,15 @@
 /**
  * free_listint2 -  function that frees a listint_t list
  *
- *@head: pointer
+ * @head: pointer
  *
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *tee;
-	if (head == NULL)
-		return;
+
+if (head == NULL)
+return;
 
 	while (*head)
 	{
