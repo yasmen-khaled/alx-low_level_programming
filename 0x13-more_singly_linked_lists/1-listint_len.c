71367a8fff37d274e4ea0eea1c4bb0661340ca5b
yasmen <yasmen.khaled2000@gmail.com>
2023-08-29 15:06:05 +0200

    listsint-len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,13 +6,15 @@
  *
  * Return: no
  *
- * description : Write a function that prints all the elements
+ * Description:function that prints all the elements
  */
 
 size_t listint_len(const listint_t *h)
 {
 	size_t no = 0;
+
 	while (h)
+
 	{
 	no++;
 
