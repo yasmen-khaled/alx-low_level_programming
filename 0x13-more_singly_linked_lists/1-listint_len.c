033f09acc63a4a8157c679df8836c1b31957e989
yasmen <yasmen.khaled2000@gmail.com>
2023-08-29 23:11:54 +0200

    listint-len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,10 @@
 #include "lists.h"
 /**
- *lisint_len -function that returns the number of elements in a linked.
- *@h: pointer
- *
+ * listint_len-returns the number of elements in a linked listint_t
+ * @h:linked list
  *
  * Return: no
  *
- * Description:function that prints all the elements
  */
 
 size_t listint_len(const listint_t *h)
