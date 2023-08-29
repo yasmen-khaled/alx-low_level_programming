7cf9031fddeff35c3b4a435b329f6e5a070fb8bc
yasmen <yasmen.khaled2000@gmail.com>
2023-08-29 22:20:43 +0200

    sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *sum_listint - unction that returns the sum of all the data (n)
+ *
+ *@head:node
+ *
+ *Return: x
+ */
+
+
+int sum_listint(listint_t *head)
+{
+	int x =0;
+	listint_t *tee = head;
+
+	while (tee)
+	{
+		x += tee->n;
+		tee =tee->next;
+	}
+
+	return (x);
+}
