4f5416e3178ad7801e186f284b37f984e7e02f26
yasmen <yasmen.khaled2000@gmail.com>
2023-08-30 02:30:31 +0200

    free-listint

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - a function that frees a listint_t list.
+ * @h: pointer
+ *
+ * Return:x
+ */
+
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t x = 0;
+	int v;
+	listint_t *tee;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		v = *h - (*h)->next;
+		if (v > 0)
+		{
+			tee = (*h)->next;
+			free(*h);
+			*h = tee;
+			x++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			x++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (x);
+}
