c661e7076820267efad668838ac6bec7d60a86c7
yasmen <yasmen.khaled2000@gmail.com>
2023-09-04 22:53:48 +0300

    read-textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *read_textfile-function that reads a text file and prints it to the POSIX
+ *@filename: file
+ *@letters: file
+ *
+ *Return:z
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t x, y, z;
+	char *no;
+
+	x = open(filename, 0_RDONLY);
+
+	if (x == -1)
+	{
+		return (0);
+	}
+	no = malloc(sizeof(char) * letters);
+
+	y = read(x, no, letters);
+	z = write(STDOUT_FILENO, no, y);
+	free(no);
+	close(x);
+	return(z);
+}
+
