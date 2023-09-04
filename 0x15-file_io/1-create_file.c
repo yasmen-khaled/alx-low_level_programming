707fbc6bb92aa21df53aa1ba8a2cd65a83c7f773
yasmen <yasmen.khaled2000@gmail.com>
2023-09-04 23:56:03 +0300

    under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *creat_fils- function that creates a file.
+ *creat_fils - function that creates a file.
  *@filename: pointer
  *@text_content: pointer
  *
@@ -11,12 +11,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int x, y, z = 0;
-	if(filename == NULL)
-		return (-1);
+
+if (filename == NULL)
+return (-1);
+
 	if (text_content != NULL)
 	{
-		for (z =0; text_content[z];)
-			z++;
+	for (z = 0; text_content[z];)
+	z++;
 	}
 	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	y = write(x, text_content, z);
