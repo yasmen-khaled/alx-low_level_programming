ae76aeb03936323792fab21360768a55eeec3aab
yasmen <yasmen.khaled2000@gmail.com>
2023-10-13 00:18:29 +0300

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,10 +10,11 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int c;
+
 	c = 0;
 
-	if(h == NULL)
-		return(c);
+	if (h == NULL)
+		return (c);
 	while (h->prev != NULL)
 		h = h->prev;
 	while (h != NULL)
