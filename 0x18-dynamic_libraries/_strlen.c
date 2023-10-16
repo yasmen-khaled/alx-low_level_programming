67a1be91f4485d6d9b75f43605cfba4b90b8c0e3
yasmen <yasmen.khaled2000@gmail.com>
2023-10-16 21:28:53 +0300

    done

---
+++ 0x18-dynamic_libraries/_strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * _strlen - function calculates the length of a string
+ *   *@str:pointer
+ *    *Return: i
+ *     */
+
+int _strlen(char *str)
+{
+		int i = 0;
+
+			while (str[i] != '\0')
+					{
+							i++;
+								}
+				return (i);
+}
