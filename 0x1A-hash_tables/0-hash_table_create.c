540345b7dd3c881b2296c8cfb4724de01e314dd9
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2023-11-04 00:59:09 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - hash table
+ * @size: array.
+ *
+ * Return: NULL
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *po;
