f137a9fe77d2e5eefef7f27c85a0df1a55845920
yasmen <yasmen.khaled2000@gmail.com>
2024-02-06 23:56:56 +0200

    binary-search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+
+int binary_search(int *array, size_t size, int value) {
+	    size_t left =  0;
+	        size_t right = size -  1;
+
+		    while (right >= left) {
+			            size_t mid = left + (right - left) /  2;
+
+				            if (array[mid] == value) {
+						                return mid;
+								        }
+
+					            if (array[mid] > value) {
+							                right = mid -  1;
+									        } else {
+											            left = mid +  1;
+												            }
+						        }
+
+		        return -1;
+}
