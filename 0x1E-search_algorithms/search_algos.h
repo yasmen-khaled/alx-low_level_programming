68cec45771c00d2854f0cd52858a84e849a11c45
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-02-07 17:02:03 +0300

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,28 +1,9 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
 
-/* LIBRARIES */
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-
-typedef struct listint_s
-{
-		int n;
-			size_t index;
-				struct listint_s *next;
-} listint_t;
-
-
-
-typedef struct skiplist_s
-{
-		int n;
-			size_t index;
-				struct skiplist_s *next;
-					struct skiplist_s *express;
-} skiplist_t;
 
 
 int linear_search(int *array, size_t size, int value);
